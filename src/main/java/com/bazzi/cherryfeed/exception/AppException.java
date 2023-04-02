c187d0a4de2984843d57b3201f2fb81b556439ea
Luca Kwak2 <cldud03@naver.com>
2023-04-02 15:04:39 +0900

    init

---
+++ src/main/java/com/bazzi/cherryfeed/exception/AppException.java
@@ -0,0 +1,15 @@
+package com.bazzi.cherryfeed.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class AppException extends RuntimeException{
+
+
+    private ErrorCode errorCode;
+    private String message;
+
+
+}
